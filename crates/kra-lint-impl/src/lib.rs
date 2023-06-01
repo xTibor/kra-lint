b71cfd6d78e65469ed00bd32f2ead500b1ab2c44
Nagy Tibor <xnagytibor@protonmail.com>
2023-06-01 18:09:32 +0200

    Reorder top level exports

--- crates/kra-lint-impl/src/lib.rs
+++ crates/kra-lint-impl/src/lib.rs
@@ -3,9 +3,12 @@ mod lint_config_fields;
 mod lint_output;
 mod lint_pass;
 
-pub use lint_config::{LintConfig, LintConfigCollection, LintConfigError};
+pub use lint_config::{LintConfig, LintConfigCollection};
 pub use lint_output::{
     LintMessages, LintMessagesCollection, LintMessagesCollectionEntry, LintMessagesEntry, LintMetadata,
-    LintOutputError, LintOutputFormat,
+    LintOutputFormat,
 };
+
+pub use lint_config::LintConfigError;
+pub use lint_output::LintOutputError;
 pub use lint_pass::LintPassError;
